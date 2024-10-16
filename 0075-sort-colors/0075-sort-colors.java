eba922653f81751a2e0e4254e7d9bd711f0ac5ce
Naman Sharma <133640321+NamanDcode@users.noreply.github.com>
2024-10-16 22:29:08 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (91.27%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -4,7 +4,6 @@ class Solution {
         int white = 0;
         int blue = 0;
         
-        // Count the number of red (0), white (1), and blue (2)
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == 0) {
                 red++;
@@ -15,16 +14,15 @@ class Solution {
             }
         }
         
-        // Overwrite the array with the correct number of 0s, 1s, and 2s
         int index = 0;
         for (int i = 0; i < red; i++) {
-            nums[index++] = 0; // Place all 0s first
+            nums[index++] = 0; 
         }
         for (int i = 0; i < white; i++) {
-            nums[index++] = 1; // Then all 1s
+            nums[index++] = 1; 
         }
         for (int i = 0; i < blue; i++) {
-            nums[index++] = 2; // Finally all 2s
+            nums[index++] = 2; 
         }
     }
 }
